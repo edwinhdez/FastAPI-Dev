c4d79738f43fc7820b7bf4e83565b701210a03ed
Edwin Hernandez <edwin.hdez@live.com>
2023-02-19 02:24:35 -0600

    validations_path

--- fast-api-hello-world/main.py
+++ fast-api-hello-world/main.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 
 # * FastAPI
 from fastapi import FastAPI
-from fastapi import Body, Query
+from fastapi import Body, Query, Path
 
 app = FastAPI()
 
@@ -30,8 +30,29 @@ def create_person(person: Person = Body(...)):
 # * Validaciones: Query Parameters
 @app.get("/person/detail")
 def show_person(
-    name: Optional[str] = Query(None, min_length = 1, max_length=50),
-    age: str = Query(...)
+    name: Optional[str] = Query(
+        None, 
+        min_length = 1, 
+        max_length=50,
+        title = 'Person Name',
+        description="This is the person name. It is between 1 and 50 chars"
+        ),
+    age: str = Query(
+        ...,
+        title="Person age",
+        description="This is the person age. It is required"
+        )
 ):
     return {name:age}
 
+# * Validaciones: Path parameters
+@app.get("/person/detail/{person_id}")
+def show_person(
+    person_id: int = Path(
+        ...,
+        gt=0,
+        title="person id",
+        description="This is the person id. It is required"
+        )
+):
+    return {person_id: "It exists"}
