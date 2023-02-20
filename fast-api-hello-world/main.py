4f91053b4d81f0420733f24a0221d9167982c178
Edwin Hernandez <edwin.hdez@live.com>
2023-02-20 08:48:15 -0600

    validations_body

--- fast-api-hello-world/main.py
+++ fast-api-hello-world/main.py
@@ -11,6 +11,11 @@ from fastapi import Body, Query, Path
 app = FastAPI()
 
 # * Models
+class Location(BaseModel):
+    city: str
+    state: str
+    country : str
+
 class Person(BaseModel):
     first_name: str
     last_name: str
@@ -56,3 +61,19 @@ def show_person(
         )
 ):
     return {person_id: "It exists"}
+
+@app.put("/person/{person_id}")
+def update_person(
+    person_id: int = Path(
+        ...,
+        title="Person ID",
+        description= "This is the person ID",
+        gt=0
+    ),
+    person: Person = Body(...),
+    location: Location = Body(...)
+):
+    result = person.dict()
+    result.update(location.dict())
+    
+    return result
